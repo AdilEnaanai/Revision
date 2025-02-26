1fa865666a4c029c4a0a5e53dcee8a8895581616
enaanai <aenaanai@uae.ac.ma>
2025-02-26 09:33:33 +0000

    Initial commit

---
+++ src/Principale.java
@@ -0,0 +1,16 @@
+
+public class Principale {
+
+	public static void main(String[] args) {
+		Position p=new Position(0, 0);
+		Robot r1=new Robot(1,"Romeo","Red",1,p,"est");
+		System.out.println("Etat initial      : "+r1);
+		r1.avancer(1);
+		System.out.println("Etat après avancer: "+r1);
+		r1.reculer(1);
+		System.out.println("Etat après reculer: "+r1);
+		
+
+	}
+
+}
